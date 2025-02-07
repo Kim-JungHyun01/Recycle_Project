bd726911d464ea126736be535e15acd86c9f5730
312-05\helena1005 <helena1005@naver.com>
2025-02-07 17:56:11 +0900

    메인페이지 공지사항 리스트업 작업 및 공지사항 리스트페이지 수정 작업

--- project/src/main/java/com/lrin/project/controller/HomeController.java
+++ project/src/main/java/com/lrin/project/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.lrin.project.controller;
 
+import com.lrin.project.entity.board.BoardEntity;
 import com.lrin.project.entity.item.Item;
+import com.lrin.project.repository.board.BoardRepository;
 import com.lrin.project.service.item.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +16,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class HomeController {
     private final ItemService itemService;
+
+    @Autowired
+    private BoardRepository boardRepository;
+
     @GetMapping(value = "/")
     public String index(Model model) {
+        List<BoardEntity> boardEntityList = this.boardRepository.findAll();
         model.addAttribute("cssPath", "home/index");
         model.addAttribute("pageTitle", "메인");
         model.addAttribute("jsPath", "home/index");
+        model.addAttribute("boardEntityList", boardEntityList);
         return "home/index";
     }
     @GetMapping(value = "/price")
