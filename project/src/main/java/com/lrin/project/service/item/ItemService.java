c63ad35cf9fd76663b6f3bb368897a289efb9001
312-02\cheol <java960318@naver.com>
2025-02-10 18:00:29 +0900

    orderList 기능 구현, orderAdmin 관리자 list 구현중

--- project/src/main/java/com/lrin/project/service/item/ItemService.java
+++ project/src/main/java/com/lrin/project/service/item/ItemService.java
@@ -1,12 +1,13 @@
 package com.lrin.project.service.item;
 
+import com.lrin.project.dto.item.ItemDTO;
 import com.lrin.project.entity.item.Item;
 
 import java.util.List;
 
 public interface ItemService {
 
-    List<Item> getItems();
+    List<ItemDTO> getItems();
 
 
 }
