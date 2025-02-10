efa05f273a0d67914e983446649747115df5a077
312-05\helena1005 <helena1005@naver.com>
2025-02-10 16:01:32 +0900

    member 카테고리 이전 버튼 onclick 이벤트 수정

--- project/src/main/resources/static/js/common/common.js
+++ project/src/main/resources/static/js/common/common.js
@@ -1,6 +1,9 @@
 let win_href = window.location.href;//페이지 전체 경로
 let win_path = window.location.pathname;//페이지 이름 경로
 let win_search = window.location.search;//페이지 ?a=b에 대한 경로
+function goBack(){
+    window.history.go(-1);
+}
 
 function pageReload(){
     location.reload(true);
