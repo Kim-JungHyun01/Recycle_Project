71bf9f4262530b44bfe0508d3731b0c57fe8ebb1
312-KimJungHyun <112wjdugs@gmail.com>
2025-02-11 13:54:47 +0900

    python config / html에 시큐리티 토큰 추가

--- project/src/main/java/com/lrin/project/config/PythonConfig.java
+++ project/src/main/java/com/lrin/project/config/PythonConfig.java
@@ -2,18 +2,34 @@ package com.lrin.project.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class PythonConfig {
+
     @Bean
-    WebClient webClient(){
-        return WebClient.builder().exchangeStrategies(ExchangeStrategies.builder()
+    WebClient webClient() {
+        return WebClient.builder()
+                .exchangeStrategies(ExchangeStrategies.builder()
                         .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(-1))
-                        .build()).baseUrl("http://localhost:8001/") //파이썬 ai 서버주소
-//                업로드한 파일을 ai 서버에 전송하기 위해서 버퍼의 크기를 제한을 제한없이(byteCount : -1) 진행
+                        .build())
+                .baseUrl("http://localhost:8001/") // 파이썬 AI 서버 주소
+                .filter(ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+                    ClientRequest filteredRequest = ClientRequest.from(clientRequest)
+                            .header("Authorization", "Bearer YOUR_TOKEN_HERE") // 여기에 토큰 추가
+                            .build();
+                    return Mono.just(filteredRequest);
+                }))
                 .build();
-    }//end WebClient
+    }
+
 
 }//end class
--- project/src/main/java/com/lrin/project/controller/PythonController.java
+++ project/src/main/java/com/lrin/project/controller/PythonController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
