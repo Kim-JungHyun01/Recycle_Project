be508f9ddbdf0223bf9b690ef1ba668b2d187a3c
312-02\cheol <java960318@naver.com>
2025-02-14 10:08:17 +0900

    수거 페이지 로그인 처리

--- project/src/main/java/com/lrin/project/config/SecurityConfiguration.java
+++ project/src/main/java/com/lrin/project/config/SecurityConfiguration.java
@@ -30,6 +30,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         String[] urlsToBePermittedAll = {
+            "/collect",
             "/contact/**",
             "/notices/**",
             "/register/**",
